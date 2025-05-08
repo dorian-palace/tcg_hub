3e6e772e41c60ef11f83f96edada14f0fcd2a6a0
Dorian Palace <mr.dpalace@gmail.com>
2025-05-08 13:13:49 +0200

    nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-[rgb(31,41,55)] text-white shadow-lg">
+<nav style="background-color: rgb(113 131 156);" class=" text-white shadow-lg">
     <div class="container mx-auto px-4">
         <div class="flex justify-between items-center h-16">
             <div class="flex items-center">
